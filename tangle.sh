a21703ac590f1f02d42edee1d98081d857523f53
tslight <tobyslight@gmail.com>
2018-06-27 19:58:35 +0100

    get rid of warning messages and display dir in my/tangle-dir output

--- tangle.sh
+++ tangle.sh
@@ -4,23 +4,23 @@ tangle_tangles () {
     emacs -Q --batch --eval '(with-current-buffer
 			     (find-file-noselect "~/etc/emacs/my-tangles.org")
 				 (org-babel-tangle))'\
-				     2>&1 | grep -Ei 'complete|error|warn'
+				     2>&1 | grep -Ei 'complete|error'
 }
 
 tangle_all () {
     emacs -Q --batch -l ~/.emacs.d/site-lisp/my-tangles.el\
-	  --eval '(my/tangle-all)' 2>&1 | grep -Ei 'complete|error|warn'
+	  --eval '(my/tangle-all)' 2>&1 | grep -Ei 'complete|error'
 }
 
 tangle_emacs () {
     emacs -Q --batch -l ~/.emacs.d/site-lisp/my-tangles.el\
-	  --eval '(my/tangle-init)' 2>&1 | grep -Ei 'complete|error|warn'
+	  --eval '(my/tangle-init)' 2>&1 | grep -Ei 'complete|error'
 }
 
 tangle_dir () {
     local dir="$1"
     emacs -Q --batch -l ~/.emacs.d/site-lisp/my-tangles.el\
-	  --eval '(my/tangle-directory "~/etc/'$dir'")' 2>&1 | grep -Ei 'complete|error|warn'
+	  --eval '(my/tangle-directory "~/etc/'$dir'")' 2>&1 | grep -Ei 'complete|error'
 }
 
 main () {
