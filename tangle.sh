9f8c0c8328129d40e21f81168c355ec0e8f1008a
tslight <tobyslight@gmail.com>
2018-06-27 19:18:37 +0100

    decrease verbosity

--- tangle.sh
+++ tangle.sh
@@ -3,20 +3,24 @@
 tangle_tangles () {
     emacs -Q --batch --eval '(with-current-buffer
 			     (find-file-noselect "~/etc/emacs/my-tangles.org")
-				 (org-babel-tangle))'
+				 (org-babel-tangle))'\
+				     2>&1 | grep -Ei 'complete|error|warn'
 }
 
 tangle_all () {
-    emacs -Q --batch -l ~/.emacs.d/site-lisp/my-tangles.el --eval '(my/tangle-all)'
+    emacs -Q --batch -l ~/.emacs.d/site-lisp/my-tangles.el\
+	  --eval '(my/tangle-all)' 2>&1 | grep -Ei 'complete|error|warn'
 }
 
 tangle_emacs () {
-    emacs -Q --batch -l ~/.emacs.d/site-lisp/my-tangles.el --eval '(my/tangle-init)'
+    emacs -Q --batch -l ~/.emacs.d/site-lisp/my-tangles.el\
+	  --eval '(my/tangle-init)' 2>&1 | grep -Ei 'complete|error|warn'
 }
 
 tangle_dir () {
     local dir="$1"
-    emacs -Q --batch -l ~/.emacs.d/site-lisp/my-tangles.el --eval '(my/tangle-directory "~/etc/'$dir'")'
+    emacs -Q --batch -l ~/.emacs.d/site-lisp/my-tangles.el\
+	  --eval '(my/tangle-directory "~/etc/'$dir'")' 2>&1 | grep -Ei 'complete|error|warn'
 }
 
 main () {
