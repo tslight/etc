99d61290b89ac8fe78056bf82584796d0dae5d6f
tslight <tobyslight@gmail.com>
2018-07-03 14:26:18 +0100

    password prompt hides anyway...

--- tangle.sh
+++ tangle.sh
@@ -4,23 +4,23 @@ tangle_tangles () {
     emacs -Q --batch --eval '(with-current-buffer
 			     (find-file-noselect "~/etc/emacs/my-tangles.org")
 				 (org-babel-tangle))'\
-				     | grep -Ei 'complete|error|password'
+				     2>&1 | grep -Ei 'complete|error'
 }
 
 tangle_all () {
     emacs -Q --batch -l ~/.emacs.d/site-lisp/my-tangles.el\
-	  --eval '(my/tangle-all)' | grep -Ei 'complete|error|password'
+	  --eval '(my/tangle-all)' 2>&1 | grep -Ei 'complete|error'
 }
 
 tangle_emacs () {
     emacs -Q --batch -l ~/.emacs.d/site-lisp/my-tangles.el\
-	  --eval '(my/tangle-init)' | grep -Ei 'complete|error|password'
+	  --eval '(my/tangle-init)' 2>&1 | grep -Ei 'complete|error'
 }
 
 tangle_dir () {
     local dir="$1"
     emacs -Q --batch -l ~/.emacs.d/site-lisp/my-tangles.el\
-	  --eval '(my/tangle-directory "~/etc/'$dir'")' | grep -Ei 'complete|error|password'
+	  --eval '(my/tangle-directory "~/etc/'$dir'")' 2>&1 | grep -Ei 'complete|error'
 }
 
 main () {
