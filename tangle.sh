081d84476b0f140855db845c70a7005bc041f606
tslight <tobyslight@gmail.com>
2018-07-03 14:11:41 +0100

    make dos junk function interactive and add password prompt to tangle script

--- tangle.sh
+++ tangle.sh
@@ -4,23 +4,23 @@ tangle_tangles () {
     emacs -Q --batch --eval '(with-current-buffer
 			     (find-file-noselect "~/etc/emacs/my-tangles.org")
 				 (org-babel-tangle))'\
-				     2>&1 | grep -Ei 'complete|error'
+				     2>&1 | grep -Ei 'complete|error|password'
 }
 
 tangle_all () {
     emacs -Q --batch -l ~/.emacs.d/site-lisp/my-tangles.el\
-	  --eval '(my/tangle-all)' 2>&1 | grep -Ei 'complete|error'
+	  --eval '(my/tangle-all)' 2>&1 | grep -Ei 'complete|error|password'
 }
 
 tangle_emacs () {
     emacs -Q --batch -l ~/.emacs.d/site-lisp/my-tangles.el\
-	  --eval '(my/tangle-init)' 2>&1 | grep -Ei 'complete|error'
+	  --eval '(my/tangle-init)' 2>&1 | grep -Ei 'complete|error|password'
 }
 
 tangle_dir () {
     local dir="$1"
     emacs -Q --batch -l ~/.emacs.d/site-lisp/my-tangles.el\
-	  --eval '(my/tangle-directory "~/etc/'$dir'")' 2>&1 | grep -Ei 'complete|error'
+	  --eval '(my/tangle-directory "~/etc/'$dir'")' 2>&1 | grep -Ei 'complete|error|password'
 }
 
 main () {
