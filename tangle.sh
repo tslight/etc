9749d6b958e6e65ab78dfc0375be0437caec9099
tslight <tobyslight@gmail.com>
2018-07-03 14:24:30 +0100

    lazygitpush

--- tangle.sh
+++ tangle.sh
@@ -4,23 +4,23 @@ tangle_tangles () {
     emacs -Q --batch --eval '(with-current-buffer
 			     (find-file-noselect "~/etc/emacs/my-tangles.org")
 				 (org-babel-tangle))'\
-				     2>&1 | grep -Ei 'complete|error|Password for'
+				     | grep -Ei 'complete|error|password'
 }
 
 tangle_all () {
     emacs -Q --batch -l ~/.emacs.d/site-lisp/my-tangles.el\
-	  --eval '(my/tangle-all)' 2>&1 | grep -Ei 'complete|error|Password for'
+	  --eval '(my/tangle-all)' | grep -Ei 'complete|error|password'
 }
 
 tangle_emacs () {
     emacs -Q --batch -l ~/.emacs.d/site-lisp/my-tangles.el\
-	  --eval '(my/tangle-init)' 2>&1 | grep -Ei 'complete|error|Password for'
+	  --eval '(my/tangle-init)' | grep -Ei 'complete|error|password'
 }
 
 tangle_dir () {
     local dir="$1"
     emacs -Q --batch -l ~/.emacs.d/site-lisp/my-tangles.el\
-	  --eval '(my/tangle-directory "~/etc/'$dir'")' 2>&1 | grep -Ei 'complete|error|Password for'
+	  --eval '(my/tangle-directory "~/etc/'$dir'")' | grep -Ei 'complete|error|password'
 }
 
 main () {
