4c13cbbe3ccf4f3abff3ab3d7624df6b593636c1
tslight <tobyslight@gmail.com>
2019-03-25 00:20:21 +0000

    attempting fibonacci fully recursively

--- emacs/elisp/fibonacci.el
+++ emacs/elisp/fibonacci.el
@@ -2,10 +2,19 @@
 ;;; Code:
 ;;; Commentary:
 
-(defun my/fibonacci(max)
-  "Get all the fibonacci numbers up MAX."
-  (interactive "nEnter a number to get fibonacci number up to: ")
-  (cond ((< max 2) max)
-	((my/fibonacci (+ (- max 1) (- max 2))))
+(defun my/fibonacci(n)
+  "Get the Nth Fibonacci number."
+  (cond ((< n 2) n)
+	((my/fibonacci (+ (- n 1) (- n 2))))
 	)
+  )
+
+(defun my/get-fibs(numbers)
+  "Get all the Fibonacci numbers up to NUMBER."
+  (cond ((number > )))
+  )
+(defun my/fibs(number)
+  "Get all the Fibonacci numbers up to NUMBER."
+  (interactive "nEnter the Fibonacci you want to go up to: ")
+  (setq fibs (my/get-fibs (number-sequence 1 number)))
   )
