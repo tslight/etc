faead4c48b9f2c194bfba451d5979bd6a1386781
tslight <tobyslight@gmail.com>
2019-03-25 23:39:31 +0000

    getting more comfy with recursion

--- emacs/elisp/fibonacci.el
+++ emacs/elisp/fibonacci.el
@@ -4,19 +4,49 @@
 
 (defun my/fibonacci(n)
   "Get the Nth Fibonacci number."
-  (cond ((< n 2) n)
-	((my/fibonacci (+ (- n 1) (- n 2))))
-	)
+  (if (< n 2)
+      n
+    (+ (my/fibonacci (- n 1))
+       (my/fibonacci (- n 2)))
+    )
   )
 
-(defun my/get-fibs(numbers)
-  "Get all the Fibonacci numbers up to NUMBER."
-  (cond ((eq (length numbers) 1)
-	 (car )))
+(defun my/factorial(n)
+  "Calculate factorial of N."
+  (if (= n 1)
+      n
+    (* n (my/factorial (- n 1)))
+    )
   )
+
+(defun my/recursive-list(range, list, function)
+  "Get recursive LIST of FUNCTION results of RANGE."
+  (if (= (length range) 1)
+      (funcall function (car range))
+    (my/recursive-list (cdr range) )
+    )
+  )
+
+(defun my/get-fibs(numbers fibonacci)
+  "Get all the FIBONACCI NUMBERS."
+  (if (equal (length numbers) 1)
+      (setq fibonacci (cons (my/fibonacci (car numbers)) fibonacci))
+    (my/get-fibs (cdr numbers) fibonacci)
+    )
+  fibonacci
+  )
+
+
+(my/factorial 18)
+(my/fibonacci 18)
+
 
 (defun my/fibs(number)
   "Get all the Fibonacci numbers up to NUMBER."
   (interactive "nEnter the Fibonacci you want to go up to: ")
-  (setq fibs (my/get-fibs (number-sequence 1 number)))
+  (setq fibs (my/get-fibs (number-sequence 1 number) (list)))
+  (message "%s" fibs)
   )
+
+(provide 'fibonacci)
+;;; fibonacci.el ends here
