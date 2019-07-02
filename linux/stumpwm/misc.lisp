3f06ffef72855e253f8fc7dc463ee3d8d4b9de6e
tslight <tslight@pm.com>
2019-07-03 00:07:37 +0100

    add group-windowlist

--- linux/stumpwm/misc.lisp
+++ linux/stumpwm/misc.lisp
@@ -4,3 +4,12 @@
 
 (defun replace-chars (sub chars str)
   (substitute-if sub (lambda (ch) (find ch chars)) str))
+
+(defcommand group-windowlist () ()
+  "Windowlist showing all windows from all groups."
+  (if-let ((window-list (sort-windows-by-number
+			 (mapcan (lambda (s) (screen-windows s)) *screen-list*))))
+    (if-let ((window (select-window-from-menu window-list *window-format*)))
+      (focus-all window)
+      (throw 'error :abort))
+    (message "No Managed Windows")))
