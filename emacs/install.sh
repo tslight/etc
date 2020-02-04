8b64e99148d331ff85753cde5b8d7fb0d6a07c41
tslight <tslight@pm.com>
2020-02-04 17:46:44 +0000

    initial commit

---
+++ emacs/install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Running this script will tangle all the code blocks from the org mode files
+# to ~/.emacs.d and load them.
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+emacs -Q \
+      --batch \
+      --eval '(org-babel-load-file "'$DIR/site-lisp/my-tangles.org'")' \
+      --eval '(my/tangle-directory "'$DIR'/")'
