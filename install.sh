1f61795bb02d7dbd5d5de213b519a0b496b7dc9b
tslight <tslight@pm.com>
2020-03-13 00:17:17 +0000

    trying to get this working with emacs 27*

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@
 # to ~/.emacs.d and load them.
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-emacs -Q \
-      --eval '(org-babel-load-file "'$DIR/emacs/site-lisp/my-tangles.org'")' \
+
+emacs --batch \
+      --eval '(org-babel-load-file "'$DIR'/emacs/site-lisp/my-tangles.org")' \
       --eval '(my/tangle-all)'
